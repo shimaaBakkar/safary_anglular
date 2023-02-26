c49f614df404480bb3b269b50305e474fe2f5bc4
shimaaBakkar <shimaaahmed1099@gmail.com>
2023-02-26 19:38:32 +0200

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomTourgideComponent } from './customize-trip/custom-tourgide/custom-tourgide.component';
+import { CustomTripDetailsComponent } from './customize-trip/custom-trip-details/custom-trip-details.component';
+import { PlaceComponent } from './customize-trip/custom_place/place.component';
+import { HotelComponent } from './customize-trip/custo_hotel/hotel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customize_trip', component: CustomTripDetailsComponent},
+  {path: 'custo_hotel', component: HotelComponent },
+  {path: 'custom_place', component: PlaceComponent},
+  {path: 'custom-tourgide', component: CustomTourgideComponent },
+  {path: 'custom-trip-details', component: CustomTripDetailsComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './temp/navbar/navbar.component';
+import {FooterComponent} from './temp/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
