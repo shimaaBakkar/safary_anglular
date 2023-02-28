9454829c6c686f1045e7b8b48c4a09dc00ad7011
shimaaBakkar <shimaaahmed1099@gmail.com>
2023-02-28 16:28:07 +0200

    updates by shimaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CustomTourgideComponent } from './customize-trip/custom-tourgide/custom-tourgide.component';
-import { CustomTripDetailsComponent } from './customize-trip/custom-trip-details/custom-trip-details.component';
-import { PlaceComponent } from './customize-trip/custom_place/place.component';
-import { HotelComponent } from './customize-trip/custo_hotel/hotel.component';
+// import { CustomTourgideComponent } from './customize-trip/custom-tourgide/custom-tourgide.component';
+// import { CustomTripDetailsComponent } from './customize-trip/custom-trip-details/custom-trip-details.component';
+// import { PlaceComponent } from './customize-trip/custom_place/place.component';
+// import { HotelComponent } from './customize-trip/custo_hotel/hotel.component';
 
 const routes: Routes = [
   {
